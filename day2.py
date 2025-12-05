c5e5aa1e9cb477c1c64eb06a422a9d4fbc2a2112
Xander <XanderG2@users.noreply.github.com>
2025-12-05 21:48:31 +0000

    day 2 part 2

--- day2.py
+++ day2.py
@@ -1,3 +1,5 @@
+import math
+
 with open("inputs/day2.txt", "r") as f:
     INPUT = f.read()
 
@@ -14,7 +16,24 @@ def partone():
                 invalidIDsum += num
     print(invalidIDsum)
 
+def parttwo():
+    ranges = INPUT.split(",")
+    invalidIDsum = 0
+    for rangE in ranges:
+        twonums = list(map(int, rangE.split("-")))
+        nums = range(twonums[0], twonums[1]+1)
+        for num in nums:
+            Num = str(num)  
+            half = math.ceil(len(Num)/2)
+            for x in range(half):
+                potStr = Num[:x+1]
+                if len(Num) % len(potStr) == 0:
+                    if Num == potStr*(len(Num)//len(potStr)) and len(Num)//len(potStr) != 1:
+                        invalidIDsum += num
+                        break
+    print(invalidIDsum)
 
 
 if __name__ == "__main__":
     partone()
+    parttwo()
