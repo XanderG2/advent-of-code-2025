a3929576afbb87d71e4c1af13313f87d9929fb78
Xander <XanderG2@users.noreply.github.com>
2025-12-05 21:05:37 +0000

    day 1 part 2

--- day1.py
+++ day1.py
@@ -14,5 +14,19 @@ def partone():
             zeros += 1
     print(zeros)
 
+def parttwo():
+    zeros = 0
+    currentNum = 50
+    for instruction in INPUT:
+        direction = instruction[0]
+        amount = int(instruction[1:])
+        i = 1 if direction == "R" else -1
+        for _ in range(amount):
+            currentNum += i
+            if str(currentNum)[-2:] == "00" or currentNum == 0: # probably like the strangest way i could think of (totally not copy pasted), but it works!
+                zeros += 1
+    print(zeros)
+
 if __name__ == "__main__":
     partone()
+    parttwo()
