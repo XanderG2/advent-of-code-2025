aa5891c8cbd4cf742bb2eb1136eab5e7def2d43b
Xander <XanderG2@users.noreply.github.com>
2025-12-05 23:27:33 +0000

    day 3 part 2

--- day3.py
+++ day3.py
@@ -16,6 +16,26 @@ def partone():
     print(totalVoltage)
 
 
+def parttwo(): # wow so readable (i have no clue what ive written but it works (at least its not my previous part 2 (it used 64+ GB of RAM and then memory errored)))
+    def findNextDig(r, x):
+        amountLeft = len(r) - x
+        availableToCheck = r[:amountLeft+1]
+        digit = max(availableToCheck)
+        indeX = r.index(digit)
+        return digit, r[indeX+1:]
+    totalVoltage = 0
+    for line in INPUT:
+        remainingStr = line.strip()
+        volt = ""
+        for x in range(12, 0, -1):
+            num, remainingStr = findNextDig(remainingStr, x)
+            volt += num
+        totalVoltage += int(volt)
+    print(totalVoltage)
+
+
+            
 
 if __name__ == "__main__":
     partone()
+    parttwo()
