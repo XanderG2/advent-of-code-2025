bf866a7d9e7b1cc2c699099438aa444607ce5977
Xander <XanderG2@users.noreply.github.com>
2025-12-11 22:13:52 +0000

    day 11 part 2

--- day11.py
+++ day11.py
@@ -1,17 +1,43 @@
 with open("inputs/day11.txt", "r") as f:
     INPUT = [x.strip() for x in f.readlines()]
 
-def loop(current, smfo):
+def loop(current, devices):
     if current == "out":
         return 1
     total = 0
-    for v in smfo[current]:
-        total += loop(v, smfo)
+    for v in devices[current]:
+        total += loop(v, devices)
     return total
 
 def partone():
-    suckmyfatone = {device: outputs.split() for device, outputs in (line.split(": ") for line in INPUT)}
-    print(loop("you", suckmyfatone))
+    devices = {device: outputs.split() for device, outputs in (line.split(": ") for line in INPUT)}
+    print(loop("you", devices))
+
+cache = {}
+
+def loop2(current, devices, dac, fft):
+    cdf = (current, dac, fft)
+    if cdf in cache:
+        return cache[cdf]
+    if current == "out":
+        result = 1 if (dac and fft) else 0
+        cache[cdf] = result
+        return result
+    if current == "dac":
+        dac = True
+    if current == "fft":
+        fft = True
+    total = 0
+    for v in devices[current]:
+        total += loop2(v, devices, dac, fft)
+    cache[cdf] = total
+    return total
+
+def parttwo():
+    devices = {device: outputs.split() for device, outputs in (line.split(": ") for line in INPUT)}
+    print(loop2("svr", devices, False, False))
+    
 
 if __name__ == "__main__":
-    partone()
+    #partone()
+    parttwo()
