17ce660d4951fd638c3dcfd1780badda6a95dde9
Xander <XanderG2@users.noreply.github.com>
2025-12-11 23:05:46 +0000

    DAY 9 PART 2!!

--- day9.py
+++ day9.py
@@ -1,3 +1,6 @@
+from itertools import combinations
+from shapely import Polygon, box
+
 with open("inputs/day9.txt", "r") as f:
     INPUT = f.readlines()
 
@@ -18,5 +21,25 @@ def partone():
                 largest = size
     print(largest)
 
+def parttwo():
+    redTiles = [tuple(int(x) for x in line.split(",")) for line in INPUT]
+    rects = []
+    for xy, ab in combinations(redTiles, 2):
+        x1 = xy[0]
+        x2 = ab[0]
+        y1 = xy[1]
+        y2 = ab[1]
+        smallX, largeX = min(x1, x2), max(x1, x2)
+        smallY, largeY = min(y1, y2), max(y1, y2)
+        rect = (smallX, smallY, largeX, largeY)
+        rects.append(rect)
+    areas = [rectSize((a, b), (x, y)) for (a, b, x, y) in rects]
+    greenareapolygon = Polygon(redTiles)
+    boxes = (box(smallX, smallY, largeX, largeY) for (smallX, smallY, largeX, largeY) in rects)
+    isboxin = [greenareapolygon.contains(x) for x in boxes]
+    squares = [d for d, s in zip(areas, isboxin) if s]
+    print(max(squares))
+
 if __name__ == "__main__":
     partone()
+    parttwo()
