19ee5ce4c2ee888c50fc76c106bb3b3c140bacd7
Xander <XanderG2@users.noreply.github.com>
2025-12-18 17:04:34 +0000

    day 2 part 2 js

--- day2.mjs
+++ day2.mjs
@@ -17,4 +17,30 @@ function partone() {
   console.log(invalidIDsum);
 }
 
+function parttwo() {
+  const ranges = INPUT.split(",");
+  let invalidIDsum = 0;
+  for (const rangeStr of ranges) {
+    const twonums = rangeStr.split("-").map(Number);
+    for (let numberInt = twonums[0]; numberInt <= twonums[1]; numberInt++) {
+      const numberStr = String(numberInt);
+      const l = Math.ceil(numberStr.length / 2);
+      for (let x = 0; x < l; x++) {
+        const potStr = numberStr.slice(0, x + 1);
+        if (numberStr.length % potStr.length == 0) {
+          if (
+            numberStr == potStr.repeat(numberStr.length / potStr.length) &&
+            numberStr.length != potStr.length
+          ) {
+            invalidIDsum += numberInt;
+            break;
+          }
+        }
+      }
+    }
+  }
+  console.log(invalidIDsum);
+}
+
 partone();
+parttwo();
